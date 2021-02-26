501b283c0a60630a392de4949eeb64d1a4c575aa
Finnick Tian <finnicktian@gmail.com>
2021-02-26 15:45:22 +1100

    springboot example and test

---
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.jenkinsdemo;
+
+import com.example.jenkinsdemo.entity.ThemeParkRide;
+import com.example.jenkinsdemo.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class);
+	}
+
+	@Bean
+	public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+		return (args) -> {
+			repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+			repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+			repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+		};
+	}
+
+}
